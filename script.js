db70192bdc123e93e0b6b0a970001a80d6a12f9a
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2025-11-16 10:28:04 +0000

    get user input calculation and store in an array

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-let firstNumber;
-let secondNumber;
-let firstOperator;
 const calculatorDisplay = document.querySelector("#calculator__display-digits");
 const calculatorKeys = document.querySelectorAll(".key-pad__key");
 const calculatorClear = document.querySelector("#key-pad__key--clear");
+let firstNumber;
+let secondNumber;
+let firstOperator;
+let calcInput = "";
+let calculation = splitCalc(calcInput);
 
 calculatorKeys.forEach((button)=>{
     button.addEventListener('click',event =>{
-       const value = event.target.textContent;
+        const value = event.target.textContent;
+
+         if(value === "AC"){
+            calcInput = "";
+            calculatorDisplay.textContent = "0";
+            return;
+        }
+
+        calcInput += value;
+        console.log(calcInput);
 
         if(calculatorDisplay.textContent === "0"){
             calculatorDisplay.textContent = value;
-       } else{
+        } else{
             calculatorDisplay.textContent += value; 
-       }
-
-       if(value === "AC"){
-        calculatorDisplay.textContent = "0";
-       }
+        }
     });
 });
 
+function splitCalc(input){
+    return input.split(""); 
+}
 
+
+
+console.log(splitCalc(calcInput));
 function operate(firstNum, firstOp, secondNum){
     return add(firstNum, secondNum);
 }
